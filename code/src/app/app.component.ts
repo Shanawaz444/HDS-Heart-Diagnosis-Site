b745d78f5b925e47795c292ce01ea262ef26fac9
Shanawaz444 <shanawaz0891@gmail.com>
2021-06-03 14:32:51 +0530

    added routing to the module

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'code';
+  constructor(private router: Router){}
+  ngOnInit(): void {
+    this.router.navigate(['main-page'])
+    
+  }
 }
