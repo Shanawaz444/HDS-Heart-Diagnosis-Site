132a36f50cfe9f7ff272926025258394508d5793
Shanawaz444 <shanawaz0891@gmail.com>
2021-06-04 16:14:32 +0530

    altered the navigation for the prededct page

--- code/src/app/main-page/main-page.component.ts
+++ code/src/app/main-page/main-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+  constructor(private router: Router){}
 
-  constructor() { }
+  verify:string ="proceed"
 
   ngOnInit(): void {
+  }
+
+  proceedfun()
+  {
+    //this.verify="proceeding"
+    this.router.navigate(['predect-page'])
   }
 
 }
