1d0b58cf9e02223957dbab85f7104342a312e617
Shanawaz444 <shanawaz0891@gmail.com>
2021-06-04 16:14:08 +0530

    changed the app module

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+//import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private router: Router){}
+  //constructor(private router: Router){}
   ngOnInit(): void {
-    this.router.navigate(['main-page'])
+    //this.router.navigate(['predect-page'])
     
   }
 }
