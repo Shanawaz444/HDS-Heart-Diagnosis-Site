dd01bd5c3c6a5afe9da413eccc8f6e31bd9d6317
Shanawaz444 <shanawaz0891@gmail.com>
2021-06-04 16:12:55 +0530

    created the predect page

---
+++ code/src/app/predect-page/predect-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PredectPageComponent } from './predect-page.component';
+
+describe('PredectPageComponent', () => {
+  let component: PredectPageComponent;
+  let fixture: ComponentFixture<PredectPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PredectPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PredectPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ code/src/app/predect-page/predect-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-predect-page',
+  templateUrl: './predect-page.component.html',
+  styleUrls: ['./predect-page.component.css']
+})
+export class PredectPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
