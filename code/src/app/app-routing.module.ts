51a3640cd642bcffa6c08d49d4f60267d7e90829
Shanawaz444 <shanawaz0891@gmail.com>
2021-06-13 13:20:42 +0530

    added the new routings

--- code/src/app/app-routing.module.ts
+++ code/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PredectPageComponent } from './predect-page/predect-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 const routes: Routes = [
   {
     path: '',redirectTo:"/main-page",pathMatch:"full"
@@ -11,7 +13,13 @@ const routes: Routes = [
   },
   {
     path:'predect-page', component: PredectPageComponent
-  }
+  },
+  {
+    path:'register-page', component:RegisterPageComponent
+  },
+  {
+    path:'login-page', component:LoginPageComponent
+  },
 ];
 
 @NgModule({
