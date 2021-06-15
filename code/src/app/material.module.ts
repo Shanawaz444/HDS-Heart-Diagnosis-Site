7da2a10a4d76491824596618fc248d280eeb4f25
Shanawaz444 <shanawaz0891@gmail.com>
2021-06-15 23:57:29 +0530

    updated the material .ts

--- code/src/app/material.module.ts
+++ code/src/app/material.module.ts
@@ -11,16 +11,17 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
 
     imports: [
         MatStepperModule,MatDividerModule,MatSliderModule,MatSelectModule,MatRadioModule,MatDatepickerModule,MatIconModule,MatInputModule,MatFormFieldModule,
-        MatButtonModule,MatDialogModule,MatProgressSpinnerModule,
+        MatButtonModule,MatDialogModule,MatProgressSpinnerModule,MatSnackBarModule
     ],
     exports: [
         MatStepperModule,MatDividerModule,MatSliderModule,MatSelectModule,MatRadioModule,MatDatepickerModule,MatIconModule,MatInputModule,MatFormFieldModule,
-        MatButtonModule,MatDialogModule,MatProgressSpinnerModule,
+        MatButtonModule,MatDialogModule,MatProgressSpinnerModule,MatSnackBarModule
     ]
 })
 export class MyMaterialModule {}
