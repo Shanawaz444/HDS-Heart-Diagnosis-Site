885da3de4828d16d31f7718164e13213691cbcd4
Shanawaz444 <shanawaz0891@gmail.com>
2021-06-16 00:00:07 +0530

    created ts file rpe

---
+++ code/src/app/register-page-errors/register-page-errors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page-errors',
+  templateUrl: './register-page-errors.component.html',
+  styleUrls: ['./register-page-errors.component.css']
+})
+export class RegisterPageErrorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
